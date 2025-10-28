5332620f8864b7666d955690b8dd159821570c72
git <github@example.com>
2025-10-28 12:58:54 +0000

    Updating env code

--- src/config.py
+++ src/config.py
@@ -2,17 +2,24 @@
 Configuration module for loading environment variables securely.
 '''
 import os
+from pathlib import Path
 
 
 def load_env():
     """Load environment variables from a .env file securely."""
-    try:
-        from dotenv import load_dotenv
-        load_dotenv(override=False)
-    except ImportError as e:
-        raise ImportError(
-            "Missing 'python-dotenv'. Install it with 'pip install python-dotenv'."
-        ) from e
+
+    env_path = Path(".env")
+
+    if env_path.exists():
+        try:
+            from dotenv import load_dotenv
+            load_dotenv(dotenv_path=env_path,
+                        override=True)
+        except ImportError as e:
+            raise ImportError(
+                "Missing 'python-dotenv'. \
+                Install it with 'pip install python-dotenv'."
+            ) from e
 
 
 def get_env_variable(key):
