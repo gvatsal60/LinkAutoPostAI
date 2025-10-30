58c9873d35d42bd713991a423af4a518c7ebfd3c
git <github@example.com>
2025-10-30 12:23:06 +0000

    Fix LinkedIn URN Generation

--- src/linkedin.py
+++ src/linkedin.py
@@ -1,30 +1,68 @@
-'''
+"""
 Module to interact with LinkedIn API for posting content.
-'''
+"""
+
 import requests
 
+REQUEST_TIMEOUT = 10  # seconds
 
-def linkedin_post_content(access_token: str, author_urn: str, message: str) -> requests.Response:
+def get_linkedin_author_urn(access_token: str) -> str:
+    """
+    Retrieve the LinkedIn author's URN using the access token.
+    Args:
+        access_token (str): LinkedIn API access token.
+    Returns:
+        str: The URN of the authenticated user.
+    Raises:
+        KeyError: If the 'sub' key is not present in the response.
+        requests.HTTPError: For HTTP errors.
+    """
+
+    url = "https://api.linkedin.com/v2/userinfo"
+
+    headers = {
+        "Authorization": f"Bearer {access_token}",
+        "X-Restli-Protocol-Version": "2.0.0",
+    }
+
+    resp = requests.get(url, headers=headers, timeout=REQUEST_TIMEOUT)
+    resp.raise_for_status()  # Raises an HTTPError for bad status codes
+
+    data = resp.json()
+    urn_key = 'sub'
+
+    if urn_key not in data:
+        raise KeyError(
+            f"LinkedIn API response does not contain 'sub' key. Response: {data}"
+        )
+
+    urn = data[urn_key]
+
+    _author_urn = f"urn:li:person:{urn}"
+
+    return _author_urn
+
+
+def linkedin_post_content(access_token: str, message: str):
     """
     Post generated content to LinkedIn.
     Args:
         access_token (str): LinkedIn API access token.
-        author_urn (str): The URN of the author (e.g., "urn:li:person:xxxx").
         message (str): The content of the LinkedIn post.
     """
 
-    req_timeout = 10  # seconds
-
-    url = 'https://api.linkedin.com/v2/ugcPosts'
+    url = "https://api.linkedin.com/v2/ugcPosts"
 
     headers = {
-        'Authorization': f"Bearer {access_token}",
-        'X-Restli-Protocol-Version': '2.0.0',
-        'Content-Type': 'application/json'
+        "Authorization": f"Bearer {access_token}",
+        "X-Restli-Protocol-Version": "2.0.0",
+        "Content-Type": "application/json",
     }
 
+    author_urn = get_linkedin_author_urn(access_token=access_token)
+
     payload = {
-        'author': author_urn if author_urn.startswith('urn:li:person:') else f"urn:li:person:{author_urn}",
+        'author': author_urn,
         'lifecycleState': 'PUBLISHED',
         'specificContent': {
             'com.linkedin.ugc.ShareContent': {
@@ -38,7 +76,5 @@ def linkedin_post_content(access_token: str, author_urn: str, message: str) -> r
     }
 
     resp = requests.post(url, headers=headers,
-                         json=payload, timeout=req_timeout)
+                         json=payload, timeout=REQUEST_TIMEOUT)
     resp.raise_for_status()  # Raises an HTTPError for bad status codes
-
-    return resp
